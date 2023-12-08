82726bb48d7aebd6f45b310a6378c5ec5412b2a5
Tales Ribeiro <taalesr@gmail.com>
2023-12-08 13:10:28 -0300

    feat: using flags instead of read input

--- build_and_push_image.sh
+++ build_and_push_image.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+help() {
+  echo "
+Usage:
+$0 [-i -h]
+
+Flags:
+  -i   Image name                           Required
+       <username>/<image_name>:<version>
+  -r   Repository url                                 Default value: DockerHub url
+  -h   Help
+"
+}
+
+while getopts i:r:t:h flag
+do
+  case "${flag}" in
+    i) image_name=${OPTARG};;
+    r) repository_url=${OPTARG};;
+    h)
+      help
+      exit 0
+  esac
+done
+
+if [[ -z $image_name ]]; then
+  echo "Flag -i is required, please check the help guide (-h)"
+  exit 1
+fi
+
 echo "Checking if you are logged in on DockerHub"
 username=$(docker info | sed '/Username:/!d;s/.* //')
 
@@ -10,16 +39,18 @@ if [[ $username == "" ]]; then
   if [[ $? -eq 1 ]]; then
     exit 1
   fi
-  username=$(docker info | sed '/Username:/!d;s/.* //')
 else
   echo "You are already logged in"
 fi
 
-echo
-read -p "Image name: " repository 
+if [[ ! -z $repository_url ]]; then
+  image_fullname="$repository_url/$image_name"
+else
+  image_fullname="$image_name"
+fi
 
-echo "Building docker image $username/$repository"
-docker build -t "$username/$repository" .
+echo "Building docker image $image_fullname"
+docker build -t "$image_fullname" .
 
 echo "Pushing docker image"
-docker push $username/$repository
+docker push $image_fullname
