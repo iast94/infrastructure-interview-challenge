ecadcd51e8f17bbcb62fba529b47d67296821b91
Tales Ribeiro <taalesr@gmail.com>
2023-12-09 23:38:46 -0300

    fix: adding an appropriate benchmarking load test

--- configure-and-test-local-env.sh
+++ configure-and-test-local-env.sh
@@ -14,6 +14,7 @@ Flags:
        (Just set if the image is private)
   -r   Helm release name                     Default value: interview
   -n   Namespace to install helm release     Default value: interview
+  -t   Execute load test
   -h   Help
 "
 }
@@ -22,7 +23,7 @@ image_name="talessrc/infrastructure-interview-app:1.0.0"
 release_name="interview"
 namespace="interview"
 
-while getopts i:U:Pr:n:h flag
+while getopts i:U:Pr:n:th flag
 do
   case "${flag}" in
     i) image_name=${OPTARG};;
@@ -31,18 +32,18 @@ do
       read -s -p "Docker password: " docker_password;;
     r) release_name=${OPTARG};;
     n) namespace=${OPTARG};;
+    t) load_test=true;;
     h)
       help
       exit 0;
   esac
 done
 
-echo
-echo "Iniating terraform..."
+echo -e "\nIniating terraform..."
 terraform -chdir=infrastructure/terraform/environments/local init
 echo
 
-echo "Creating infrastructure..."
+echo -e "Creating infrastructure... \n"
 terraform -chdir=infrastructure/terraform/environments/local apply -auto-approve
 echo
 
@@ -54,19 +55,16 @@ imagePullSecret.username=$docker_username,\
 imagePullSecret.password=$docker_password"
 fi
 
-echo "Installing application stack..."
+echo -e "Installing application stack... \n"
 helm upgrade -n $namespace --create-namespace --wait \
 --install $release_name ./infrastructure/helm \
---set app.image=$image_name,webserver.port=80 $image_pull_secret
+--set app.image=$image_name $image_pull_secret
 echo
 
-webserver_url=$(kubectl get svc -n $namespace -o \
-custom-columns=:.metadata.name --no-headers -l \
-component="webserver-service")
-
-echo "Testing stack scalability and availability during load peaks (press ctrl+c to stop)"
-kubectl run -i --tty load-generator --rm -n $namespace \
---image=busybox:1.28 --restart=Never -- \
-/bin/sh -c "while sleep 0.01; do wget -q -O- http://$webserver_url/posts; done"
-
-kubectl delete po -n $namespace load-generator
+if [[ -n $load_test ]]; then
+  echo -e "Testing stack scalability and availability during load peaks \n"
+  docker run --network host -i httpd:2.4-alpine3.18 \
+  ab -c 350 -n 20000 http://localhost/posts
+else
+  echo "Skipping load test..."
+fi
